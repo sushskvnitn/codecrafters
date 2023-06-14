173913f5becfa7e9f4100db1c6d9ec2482071eb8
sushskvnitn <sushantkogurwar@gmail.com>
2023-06-14 19:12:22 +0530

    first commit

---
+++ components/testimonials.tsx
@@ -0,0 +1,76 @@
+import Image from 'next/image'
+
+import TestimonialImage01 from '@/public/images/testimonial-01.jpg'
+import TestimonialImage02 from '@/public/images/testimonial-02.jpg'
+import TestimonialImage03 from '@/public/images/testimonial-03.jpg'
+
+export default function Testimonials() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+
+                {/* Section header */}
+      <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+        <h2 className="h2 mb-4">Don't take our word for it</h2>
+        <p className="text-xl text-gray-400">Read what our subscribers have to say about our channel:</p>
+      </div>
+
+      {/* Testimonials */}
+      <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
+
+        {/* 1st testimonial */}
+        <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up">
+          <div>
+            <div className="relative inline-flex flex-col mb-4">
+              <Image className="rounded-full" src={TestimonialImage01} width={48} height={48} alt="Testimonial 01" />
+              <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+              </svg>
+            </div>
+          </div>
+          <blockquote className="text-lg text-gray-400 grow">— codeCrafters' tutorials have been a game-changer for me. The explanations are clear and concise, making it easy to understand complex coding concepts. I highly recommend this channel to anyone looking to improve their coding skills.</blockquote>
+          <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+            <cite className="text-gray-200 not-italic">Dwee</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">Software Developer</a>
+          </div>
+        </div>
+
+        {/* 2nd testimonial */}
+        <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="200">
+          <div>
+            <div className="relative inline-flex flex-col mb-4">
+              <Image className="rounded-full" src={TestimonialImage02} width={48} height={48} alt="Testimonial 02" />
+              <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+              </svg>
+            </div>
+          </div>
+          <blockquote className="text-lg text-gray-400 grow">— As a beginner in coding, codeCrafters' channel has been invaluable in helping me learn programming concepts effectively. The tutorials are well-explained, and the examples provided are practical and relevant. Thanks to codeCrafters, I've gained the confidence to tackle coding challenges with ease.</blockquote>
+          <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+            <cite className="text-gray-200 not-italic">Tarun</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">Aspiring Developer</a>
+          </div>
+        </div>
+
+        {/* 3rd testimonial */}
+        <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="400">
+          <div>
+            <div className="relative inline-flex flex-col mb-4">
+              <Image className="rounded-full" src={TestimonialImage03} width={48} height={48} alt="Testimonial 03" />
+              <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+              </svg>
+            </div>
+          </div>
+          <blockquote className="text-lg text-gray-400 grow">— codeCrafters has completely transformed the way I approach coding challenges. The step-by-step problem-solving techniques and explanations have greatly enhanced my problem-solving skills. I highly recommend this channel to anyone looking to level up their coding game.</blockquote>
+          <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+            <cite className="text-gray-200 not-italic">Divya</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">Experienced Developer</a>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</section>
+  )
+}
---
+++ components/ui/header.tsx
@@ -0,0 +1,63 @@
+import Link from 'next/link'
+import MobileMenu from './mobile-menu'
+
+export default function Header() {
+  return (
+    <header className="absolute w-full z-30">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="flex items-center justify-between h-20">
+          <div className="shrink-0 mr-4">
+            <Link href="/" className="block" aria-label="Cruip">
+              <img className="h-8 w-auto" data-aos="fade-up" data-aos-delay="400" src="https://i.postimg.cc/2ynL0yxz/Modern-Coding-Technology-Logo.png" style={{
+                width: '80px',
+                height: '80px',
+                backgroundColor: 'white',
+                borderRadius: '50%',
+                padding: '5px',
+                boxShadow: '0 0 10px rgba(0,0,0,0.2)',
+                marginTop: '50px'
+              }} alt="Logo" />
+            </Link>
+          </div>
+
+          {/* Desktop navigation */}
+          <nav className="hidden md:flex md:grow">
+            <ul className="flex grow justify-end flex-wrap items-center">
+              {/* <li>
+                <Link
+                  href="/signin"
+                  className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">
+                  Sign up
+                </Link>
+              </li> */}
+              <li>
+                <Link href="/blog" className="font-medium text-gray-400 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link href="/services" className="font-medium text-gray-400 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link href="https://www.sushantkogurwar.website" className="font-medium text-gray-400 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <MobileMenu />
+
+        </div>
+      </div>
+    </header>
+  )
+}
