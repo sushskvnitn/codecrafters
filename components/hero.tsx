7b2ac834b09e63e8db66f2a3a3f367140b4b817a
sushskvnitn <sushantkogurwar@gmail.com>
2023-06-14 23:24:59 +0530

    updated hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -28,7 +28,7 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Unlock the power of coding and let your creativity soar. Join us at CodeCrafters, where we inspire, educate, and empower coding enthusiasts to craft innovative solutions. Together, let's turn lines of code into works of art</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" target="_blank"  href="https://youtube.com/@CodeCrafters25933">View our Channel</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" target="_blank"  href="https://youtube.com/@CodeCrafters25933?sub_confirmation=1">View our Channel</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" target="_blank" href="https://www.sushantkogurwar.website/">Owners Website</a>
