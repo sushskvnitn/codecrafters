0d84cd96b4e0f926d7d2687271dc03a4a937a66b
sushskvnitn <sushantkogurwar@gmail.com>
2023-06-20 21:08:12 +0530

    updated dynamic youtube playlist rendering

---
+++ app/api/hello/getytvdos.ts
@@ -0,0 +1,7 @@
+export async function getyvdos(): Promise<any[]> {
+    const playlistid ='PLtBj3tlLeGLgDO7D6GuGvcoCcEZwO0CL5'
+    const API_KEY = process.env.REACT_APP_API_KEY ;
+    return await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=${playlistid}&key=${API_KEY}`)
+    .then(res=>res.json())
+    .then(data => data.items);
+}
--- components/blog.tsx
+++ components/blog.tsx
@@ -1,37 +1,54 @@
-import React from 'react';
+"use client"
+import * as React from 'react';
 import 'tailwindcss/tailwind.css';
-const Blog = () => {
-    const videos = [
-      'https://www.youtube.com/embed/1xBeRKQtEMk',
-      'https://www.youtube.com/embed/E95ubQ5w3TM',
-      'https://www.youtube.com/embed/Let7WsioITI',
-      'https://www.youtube.com/embed/usPjmbHr3JU',
-      'https://www.youtube.com/embed/oeQZ27RL4t8',
-      'https://www.youtube.com/embed/i5OLyn1IPaM',
-    ];
-  
-    return (
-      <>
-        <h1 className="text-3xl font-bold mb-4 mx-20">Video Tutorials</h1>
+import { getyvdos } from '../app/api/hello/getytvdos';
+type VideoBlockProps = {
+  id: string;
+};
+
+// Define the VideoElement component
+const VideoElement = (props: VideoBlockProps): React.ReactElement => (
+  <div className="mx-5">
+    <iframe
+      width="450"
+      height="280"
+      src={`https://www.youtube.com/embed/${props.id}`}
+      allow="autoplay; encrypted-media"
+      allowFullScreen
+      title="YouTube Video"
+    />
+  </div>
+);
+
+const Blog = (): React.ReactElement => {
+  const [videoData, setVideoData] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    const fetchVideoData = async () => {
+      try {
+        const data = await getyvdos();
+        const videoIds = data.map((item: any) => item.contentDetails.videoId    );
+        setVideoData(videoIds);
+      } catch (error) {
+        console.error('Error fetching video data:', error);
+      }
+    };
+
+    fetchVideoData();
+  }, []);
+
+  return (<>
+<h1 className="text-3xl font-bold mb-4 mx-20">Video Tutorials</h1>
         <div className="slider-container ">
           <div className="slider flex ">
-            {videos.map((video) => (
-              <div className="slider-item" key={video}>
-                <iframe
-                  src={video.replace('watch?v=', 'embed/')}
-            
-                  allow="autoplay; encrypted-media"
-                  allowFullScreen
-                  title="YouTube Video"
-                  className="w-full h-40 md:h-60 lg:h-80 xl:h-96 rounded-lg"
-                />
-              </div>
-            ))}
+        {videoData.map((videoId: string, index: number) => (
+          <div className="slider-item" key={videoId}>
+          <VideoElement key={index} id={videoId} />
           </div>
+        ))}
+         </div>
         </div>
-      </>
-    );
-  };
-  
-  export default Blog;
-  
+    </>
+  );
+};
+export default Blog;
